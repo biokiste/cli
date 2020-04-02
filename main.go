95abcdbbe0ba342a29acc5beb371ac6446089be0
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2020-04-02 18:19:57 +0200

    basic structure

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+)
+
+func init() {
+
+	configRoot, _ := os.Getwd()
+	var configPath = flag.String("config", configRoot, "defines path to config file")
+
+	flag.Parse()
+
+	// setup config file
+	viper.SetConfigName("config")    // name of config file (without extension)
+	viper.AddConfigPath(*configPath) // path to look for the config file in
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Couldn't find config.toml!")
+		log.Fatal(err)
+	}
+
+}
+
+func main() {
+
+	// create db instance to deprecated db
+	dbOld, err := sql.Open("mysql", viper.GetString("connectionDeprecatedDB"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbOld.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbOld.Close()
+
+	// create db instance to actual db
+	db, err := sql.Open("mysql", viper.GetString("connectionDB"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	results, err := dbOld.Query(`
+	SELECT
+			id, 
+			COALESCE(username, '') as username,
+			email, lastname, firstname, mobile, need_sms,
+			street, zip, city, 
+			COALESCE(date_of_birth, '') as date_of_birth,			
+			COALESCE(date_of_entry, '') as date_of_entry,
+			COALESCE(date_of_exit, '') as date_of_exit,
+			state, credit, credit_date, credit_comment,
+			COALESCE(iban, '') as iban,
+			COALESCE(bic, '') as bic,
+			COALESCE(sepa, '') as sepa,
+			COALESCE(additionals, '') as additionals, 
+			COALESCE(comment, '') as comment,
+			COALESCE(group_comment, '') as group_comment,
+			created_at, updated_at,
+			COALESCE(last_login, '') as last_login
+		FROM users`)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer results.Close()
+
+	var users []UserDeprecated
+	for results.Next() {
+		var user UserDeprecated
+		err = results.Scan(
+			&user.ID,
+			&user.Username,
+			&user.Email,
+			&user.Lastname,
+			&user.Firstname,
+			&user.Mobile,
+			&user.NeedSMS,
+			&user.Street,
+			&user.ZIP,
+			&user.City,
+			&user.DateOfBirth,
+			&user.DateOfEntry,
+			&user.DateOfExit,
+			&user.State,
+			&user.Credit,
+			&user.CreditDate,
+			&user.CreditComment,
+			&user.IBAN,
+			&user.BIC,
+			&user.SEPA,
+			&user.Additionals,
+			&user.Comment,
+			&user.GroupComment,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+			&user.LastLogin,
+		)
+		if err != nil {
+			fmt.Println(err)
+		}
+		users = append(users, user)
+	}
+	for _, usr := range users {
+		oldID := usr.ID
+		newUser := User{
+			FirstName: usr.Firstname,
+			LastName:  usr.Lastname,
+			Email:     usr.Email,
+			Phone:     usr.Mobile,
+			Street:    usr.Street,
+			// StreetNumber: usr.Street, // TODO: split Street to get Number ?
+			Zip:             usr.ZIP,
+			Birthday:        usr.DateOfBirth,
+			EntranceDate:    usr.DateOfEntry,
+			LeavingDate:     usr.DateOfExit,
+			AdditionalInfos: usr.Comment,
+			LastActivityAt:  usr.LastLogin,
+			CreatedAt:       usr.CreatedAt,
+			UpdatedAt:       usr.UpdatedAt,
+		}
+
+		fmt.Println(oldID, newUser)
+	}
+}
---
+++ types.go
@@ -0,0 +1,58 @@
+package main
+
+// UserDeprecated holds properties of old user
+type UserDeprecated struct {
+	ID            int     `json:"id"`
+	Username      string  `json:"username,omitempty"`
+	Email         string  `json:"email,omitempty"`
+	Lastname      string  `json:"lastname,omitempty"`
+	Firstname     string  `json:"firstname,omitempty"`
+	Mobile        string  `json:"mobile,omitempty"`
+	NeedSMS       int     `json:"need_sms,omitempty"`
+	Phone         string  `json:"phone,omitempty"`
+	Street        string  `json:"street,omitempty"`
+	ZIP           string  `json:"zip,omitempty"`
+	City          string  `json:"city,omitempty"`
+	DateOfBirth   string  `json:"date_of_birth,omitempty"`
+	DateOfEntry   string  `json:"date_of_entry,omitempty"`
+	DateOfExit    string  `json:"date_of_exit,omitempty"`
+	State         int     `json:"state,omitempty"`
+	Credit        float32 `json:"credit,omitempty"`
+	CreditDate    string  `json:"credit_date,omitempty"`
+	CreditComment string  `json:"credit_comment,omitempty"`
+	IBAN          string  `json:"iban,omitempty"`
+	BIC           string  `json:"bic,omitempty"`
+	SEPA          string  `json:"sepa,omitempty"`
+	RememberToken string  `json:"remember_token,omitempty"`
+	Additionals   string  `json:"additionals,omitempty"`
+	Comment       string  `json:"comment,omitempty"`
+	GroupComment  string  `json:"group_comment,omitempty"`
+	CreatedAt     string  `json:"created_at,omitempty"`
+	UpdatedAt     string  `json:"updated_at,omitempty"`
+	LastLogin     string  `json:"last_login,omitempty"`
+	Password      string  `json:"password,omitempty"`
+}
+
+// User hold props of new user
+type User struct {
+	ID              int    `json:"id"`
+	State           string `json:"state"`
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	Email           string `json:"email"`
+	Phone           string `json:"phone"`
+	Street          string `json:"street"`
+	StreetNumber    string `json:"streetNumber"`
+	Zip             string `json:"zip"`
+	Country         string `json:"country"`
+	Birthday        string `json:"birthday"`
+	EntranceDate    string `json:"entranceDate"`
+	LeavingDate     string `json:"leavingDate,omitempty"`
+	AdditionalInfos string `json:"additionalInfos,omitempty"`
+	LastActivityAt  string `json:"lastActivityAt,omitempty"`
+	CreatedAt       string `json:"createdAt"`
+	CreatedBy       int    `json:"createdBy"`
+	UpdatedAt       string `json:"updatedAt,omitempty"`
+	UpdatedBy       int    `json:"updatedBy,omitempty"`
+	UpdateComment   string `json:"updateComment,omitempty"`
+}
